6a177355e4c696414ac5e3fe0491e16b9313c3d2
hannes <hannes101@github>
2015-12-11 09:35:54 +0100

    Additional changes to the application file

--- LeeStrazizichApplication.R
+++ LeeStrazizichApplication.R
@@ -1,7 +1,13 @@
 # Application of the basic Lee-Strazizich test without any parallelization
 # Definition of values to be used in the application 
+# Number of possible structural breaks
+myBreaks <- 2
+# Assumed break in the series, "crash" - break in intercept; "break" - break in intercept and trend
+myModel <- "break"
+# Number of lags to be used in fixed specification or maximum number of lags, when using the GTOS method
+myLags <- 5
 
-myLS_test <- ur.ls(y=y , model = "crash", breaks = 1, lags = NULL, method = "GTOS",pn = 0.1, print.results = "print" )
+myLS_test <- ur.ls(y=y , model = myModel, breaks = myBreaks, lags = myLags, method = "GTOS",pn = 0.1, print.results = "print" )
 
 #Application of the parallelized Lee-Strazizich test
 library(foreach)
@@ -11,4 +17,4 @@ library(parallel)
 cl <- makeCluster(max(1, detectCores() - 1))
 registerDoSNOW(cl)
 
-myParallel_LS <- ur.ls.bootstrap(y=y , model = "crash", breaks = 1, lags = 5, method = "Fixed",pn = 0.1, critval = "bootstrap", print.results = "print")
+myParallel_LS <- ur.ls.bootstrap(y=y , model = myModel, breaks = myBreaks, lags = myLags, method = "Fixed",pn = 0.1, critval = "bootstrap", print.results = "print")
